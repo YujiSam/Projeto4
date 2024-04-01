cd83af89152515400d940cb12c43cb1a5cda520e
Yuji <gustavo_osugi@outlook.com>
2024-04-01 09:00:25 -0300

    Criando o Script do Calculo

---
+++ script.js
@@ -0,0 +1,6 @@
+function calc(){
+    let num1 =parseInt(document.getElementById('num1').value);
+    let num2 =parseInt(document.getElementById('num2').value);
+
+    document.getElementById("resultado").innerHTML = num1 * num2;
+}
